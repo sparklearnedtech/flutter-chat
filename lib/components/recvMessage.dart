f809a545ec30fc742e7fcd6d36416b0224ab6d1c
Christian Salvadora <christianrsalvadora@gmail.com>
2023-05-22 05:20:42 +0800

    Added recv message component(copied)

---
+++ lib/components/recvMessage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class RecvMessage extends StatelessWidget {
+  final String sender;
+  final String message;
+  const RecvMessage({super.key, required this.sender, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
+        child: Card(
+          color: Colors.green,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  sender,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(message),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
