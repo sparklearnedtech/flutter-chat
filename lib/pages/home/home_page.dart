49a29cba921b62db95b2545453e5ee57e5b8dc45
Christian Salvadora <christianrsalvadora@gmail.com>
2023-05-24 10:20:41 +0800

    Include profile component

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maritech/components/profile.dart';
 import 'package:maritech/pages/chat/chat_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,67 +20,74 @@ class _HomePageState extends State<HomePage> {
           child: Text("Home Page"),
         ),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () => {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                title: const Text("Please enter your name"),
-                content: Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    controller: nameController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 3) {
-                        return "Please enter your name";
-                      } else {
-                        return null;
-                      }
-                    },
+      body: Column(
+        children: [
+          Profile(),
+          Center(
+            child: TextButton(
+              onPressed: () => {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text("Please enter your name"),
+                    content: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        controller: nameController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 3) {
+                            return "Please enter your name";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text(
+                          "Cancel",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            String name = nameController.text;
+                            nameController.clear();
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChatPage(
+                                  name: name,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text(
+                          "Submit",
+                          style: TextStyle(fontSize: 16, color: Colors.blue),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text(
-                      "Cancel",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        String name = nameController.text;
-                        nameController.clear();
-                        Navigator.pop(context);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ChatPage(
-                              name: name,
-                            ),
-                          ),
-                        );
-                      }
-                    },
-                    child: const Text(
-                      "Submit",
-                      style: TextStyle(fontSize: 16, color: Colors.blue),
-                    ),
-                  ),
-                ],
+              },
+              child: const Text(
+                "Chat Page",
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontSize: 20,
+                ),
               ),
             ),
-          },
-          child: const Text(
-            "Chat Page",
-            style: TextStyle(
-              color: Colors.amber,
-              fontSize: 20,
-            ),
           ),
-        ),
+        ],
       ),
     );
   }
