d7cb53c166fa696d76bc70b95d5ce61a113624fd
Christian Salvadora <christianrsalvadora@gmail.com>
2023-05-24 12:12:44 +0800

    Required fields

--- lib/helpers/userModel.dart
+++ lib/helpers/userModel.dart
@@ -4,4 +4,11 @@ class UserModel {
   String email = '';
   bool isMale = false;
   String picture = '';
+  UserModel({
+    required this.fName,
+    required this.lName,
+    required this.email,
+    required this.isMale,
+    required this.picture,
+  });
 }
